c2bcc2132b4b862375384651635f1920229739e2
MaryamDev <m.danehvash2014@gmail.com>
2023-08-09 16:29:41 +0330

    initialize

---
+++ .history/src/pages/home/index_20230809162531.tsx
@@ -0,0 +1,35 @@
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+
+const Home = () => {
+  return (
+    <Grid container spacing={6} >
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader sx={{ '& .css-x8z8fw-MuiTypography-root': { color: 'red' } }} title='مشخصات فردی'></CardHeader>
+          <CardContent>
+            <Typography sx={{ mb: 2 }}></Typography>
+            
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='ACL and JWT 🔒'></CardHeader>
+          <CardContent>
+            <Typography sx={{ mb: 2 }}>
+              
+            </Typography>
+            <Typography>Please read our Authentication and ACL Documentations to get more out of them.</Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Home
---
+++ .history/src/pages/home/index_20230809162749.tsx
@@ -0,0 +1,45 @@
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+
+const Home = () => {
+  return (
+    <Grid container spacing={6} >
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader sx={{ '& .css-x8z8fw-MuiTypography-root': { color: 'red' } }} title='🚀مشخصات فردی'></CardHeader>
+          <CardContent>
+            <Typography sx={{ mb: 2 }}></Typography>
+
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader sx={{ '& .css-x8z8fw-MuiTypography-root': { color: 'red' } }} title='🔒مشخصات تکمیلی'></CardHeader>
+          <CardContent>
+            <Typography sx={{ mb: 2 }}>
+
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader sx={{ '& .css-x8z8fw-MuiTypography-root': { color: 'red' } }} title='🔒مشخصات تکمیلی'></CardHeader>
+          <CardContent>
+            <Typography sx={{ mb: 2 }}>
+
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Home
